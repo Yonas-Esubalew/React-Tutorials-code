807fff450269a6400a1226e9e1fea5f085175888
Yonas-Esubalew <157085551+Yonas-Esubalew@users.noreply.github.com>
2024-04-17 01:08:37 +0300

    Add files via upload

--- Header.js
+++ Header.js
@@ -1,48 +1,103 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+'use client';
+import {CartContext} from "@/components/AppContext";
+import Bars2 from "@/components/icons/Bars2";
+import ShoppingCart from "@/components/icons/ShoppingCart";
+import {signOut, useSession} from "next-auth/react";
+import Link from "next/link";
+import {useContext, useState} from "react";
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+function AuthLinks({status, userName}) {
+  if (status === 'authenticated') {
+    return (
+      <>
+        <Link href={'/profile'} className="whitespace-nowrap">
+          Hello, {userName}
+        </Link>
+        <button
+          onClick={() => signOut()}
+          className="bg-primary rounded-full text-white px-8 py-2">
+          Logout
+        </button>
+      </>
+    );
+  }
+  if (status === 'unauthenticated') {
+    return (
+      <>
+        <Link href={'/login'}>Login</Link>
+        <Link href={'/register'} className="bg-primary rounded-full text-white px-8 py-2">
+          Register
+        </Link>
+      </>
+    );
+  }
+}
 
-const Header = () =>  (
-  <Container>
-    <Div1>
-      <Link href="/">
-        <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
-          <DiCssdeck size="3rem" /> <span>Portfolio</span>
-        </a>
-      </Link>
-    </Div1>
-    <Div2>
-      <li>
-        <Link href="#projects">
-          <NavLink>Projects</NavLink>
+export default function Header() {
+  const session = useSession();
+  const status = session?.status;
+  const userData = session.data?.user;
+  let userName = userData?.name || userData?.email;
+  const {cartProducts} = useContext(CartContext);
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+  if (userName && userName.includes(' ')) {
+    userName = userName.split(' ')[0];
+  }
+  return (
+    <header>
+      <div className="flex items-center md:hidden justify-between">
+        <Link className="text-primary font-semibold text-2xl" href={'/'}>
+          ST PIZZA
         </Link>
-      </li>
-      <li>
-        <Link href="#tech">
-          <NavLink>Technologies</NavLink>
-        </Link>
-      </li>        
-      <li>
-        <Link href="#about">
-          <NavLink>About</NavLink>
-        </Link>
-      </li>        
-    </Div2>
-      <Div3>
-        <SocialIcons href="https://google.com">
-          <AiFillGithub size="3rem" />
-        </SocialIcons>
-        <SocialIcons href="https://google.com">
-          <AiFillLinkedin size="3rem" />
-        </SocialIcons>
-        <SocialIcons href="https://google.com">
-          <AiFillInstagram size="3rem"/>
-        </SocialIcons>
-      </Div3>
-    </Container>
-);
-
-export default Header;
+        <div className="flex gap-8 items-center">
+          <Link href={'/cart'} className="relative">
+            <ShoppingCart />
+            {cartProducts?.length > 0 && (
+              <span className="absolute -top-2 -right-4 bg-primary text-white text-xs py-1 px-1 rounded-full leading-3">
+            {cartProducts.length}
+          </span>
+            )}
+          </Link>
+          <button
+            className="p-1 border"
+            onClick={() => setMobileNavOpen(prev => !prev)}>
+            <Bars2 />
+          </button>
+        </div>
+      </div>
+      {mobileNavOpen && (
+        <div
+          onClick={() => setMobileNavOpen(false)}
+          className="md:hidden p-4 bg-gray-200 rounded-lg mt-2 flex flex-col gap-2 text-center">
+          <Link href={'/'}>Home</Link>
+          <Link href={'/menu'}>Menu</Link>
+          <Link href={'/#about'}>About</Link>
+          <Link href={'/#contact'}>Contact</Link>
+          <AuthLinks status={status} userName={userName} />
+        </div>
+      )}
+      <div className="hidden md:flex items-center justify-between">
+        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+          <Link className="text-primary font-semibold text-2xl" href={'/'}>
+            ST PIZZA
+          </Link>
+          <Link href={'/'}>Home</Link>
+          <Link href={'/menu'}>Menu</Link>
+          <Link href={'/#about'}>About</Link>
+          <Link href={'/#contact'}>Contact</Link>
+        </nav>
+        <nav className="flex items-center gap-4 text-gray-500 font-semibold">
+          <AuthLinks status={status} userName={userName} />
+          <Link href={'/cart'} className="relative">
+            <ShoppingCart />
+            {cartProducts?.length > 0 && (
+              <span className="absolute -top-2 -right-4 bg-primary text-white text-xs py-1 px-1 rounded-full leading-3">
+            {cartProducts.length}
+          </span>
+            )}
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
--- Hero.js
+++ Hero.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import phoneImg from './images/phone.svg';
-import { useGlobalContext } from './context';
+import Right from "@/components/icons/Right";
+import Image from "next/image";
 
-const Hero = () => {
-  const { closeSubmenu } = useGlobalContext();
+export default function Hero() {
   return (
-    <section className='hero' onMouseOver={closeSubmenu}>
-      <div className='hero-center'>
-        <article className='hero-info'>
-          <h1>
-            Payments infrastructure <br />
-            for the internet
-          </h1>
-          <p>
-            Millions of companies of all sizes—from startups to Fortune 500s—use
-            Stripe’s software and APIs to accept payments, send payouts, and
-            manage their businesses online.
-          </p>
-          <button className='btn'>Start now</button>
-        </article>
-        <article className='hero-images'>
-          <img src={phoneImg} className='phone-img' alt='phone' />
-        </article>
+    <section className="hero md:mt-4">
+      <div className="py-8 md:py-12">
+        <h1 className="text-4xl font-semibold">
+          Everything<br />
+          is better<br />
+          with a&nbsp;
+          <span className="text-primary">
+            Pizza
+          </span>
+        </h1>
+        <p className="my-6 text-gray-500 text-sm">
+          Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life
+        </p>
+        <div className="flex gap-4 text-sm">
+          <button className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
+            Order now
+            <Right />
+          </button>
+          <button className="flex items-center border-0 gap-2 py-2 text-gray-600 font-semibold">
+            Learn more
+            <Right />
+          </button>
+        </div>
+      </div>
+      <div className="relative hidden md:block">
+        <Image src={'/pizza.png'} layout={'fill'} objectFit={'contain'} alt={'pizza'} />
       </div>
     </section>
   );
-};
-
-export default Hero;
+}
